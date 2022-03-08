c54006e6ff9f57666f82960882c6707c23fae79f
Wendelzin10 <wendelcaua.a.g@gmail.com>
2022-03-08 17:14:31 -0300

    criado funções básicas

--- funcoes.js
+++ funcoes.js
@@ -1,4 +1,5 @@
 var numSort =[]
+var numDig = []
  function sorteioNum(){
     numSort = []
      do {
@@ -10,5 +11,29 @@ var numSort =[]
 
      } while (numSort.length < 6);
 
-     console.log(numSort)
+     document.getElementById("numsort").innerHTML = numSort
+     vereficaAcertos()
  }
+
+ function getNum(valor,pos){
+    valor = Number (valor)
+     if (valor>60){
+        alert("O valor é maior que 60")
+     }else if (valor<=0){
+        alert("O valor não pode ser menor que 0")
+     }else if(numDig.includes(valor)){
+        alert("Este valor já foi digitado")
+     }else{
+        numDig[pos] = valor
+     }
+ }
+
+  function vereficaAcertos(){
+     let cont = 0
+     numDig.forEach(function(valor,index){
+        if(numSort.includes(valor)){
+             cont = cont+1
+        }    
+     })
+     document.getElementById("total").innerText = cont
+  }
